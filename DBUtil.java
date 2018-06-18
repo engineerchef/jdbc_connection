5b469c37d4822adfce1f59211f84a757824d5590
Tolga DURAN <tolgaduran1980@gmail.com>
2018-06-18 17:35:36 +0300

    Update DBUtil.java

--- DBUtil.java
+++ DBUtil.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String OracleUser = "hr"; // Use username for Oracle Server
-    private static final String OraclePass = "hr"; // Use password for Oracle Server
+    private static final String OracleUser = "username"; // Use username for Oracle Server
+    private static final String OraclePass = "password"; // Use password for Oracle Server
     private static final String ORACLEURL = "jdbc:oracle:thin:@localhost:1521:xe";
 
-    private static final String MYSQLUser = "root"; //use username for MYSQL Server
-    private static final String MYSQLPass = "1955"; // use password for MYSQL Server
+    private static final String MYSQLUser = "username"; //use username for MYSQL Server
+    private static final String MYSQLPass = "password"; // use password for MYSQL Server
     private static final String MYSQLURL = "jdbc:mysql://localhost:3306/world?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
     public static Connection getConnection(DBSets sets) throws SQLException {

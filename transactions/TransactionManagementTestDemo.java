882b22a6f650913588e098f1ba8ce0ea5e0daed1
Tolga DURAN <tolgaduran1980@gmail.com>
2018-06-18 18:24:05 +0300

    Add files via upload

---
+++ transactions/TransactionManagementTestDemo.java
@@ -0,0 +1,79 @@
+package com.tolgaduran.jdbc.transactions;
+
+import com.tolgaduran.jdbc.DBSets;
+import com.tolgaduran.jdbc.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/*
+create table PSBANK
+(
+  ACNO   NUMBER(16) default NULL,
+  AMOUNT NUMBER     default NULL
+)
+*/
+
+public class TransactionManagementTestDemo {
+
+    public static void main(String[] args) throws SQLException{
+        // TODO Auto-generated method stub
+        try{
+            Connection conn = DBUtil.getConnection(DBSets.ORACLE);
+            conn.setAutoCommit(false);
+
+            PreparedStatement pstmt = null;
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("PSBank Transactions");
+            System.out.println("----------------------");
+            System.out.print("Enter From Account Number\t: ");
+            int fromAcno = Integer.parseInt(scanner.nextLine());
+            System.out.print("Enter To Account Number\t\t: ");
+            int toAcno = Integer.parseInt(scanner.nextLine());
+            System.out.print("Enter Amount To Transfer\t: ");
+            double amount = Double.parseDouble(scanner.nextLine());
+
+            String withdrawSQL = "Update PSBank set Amount = Amount - ? where Acno = ?";
+            pstmt = conn.prepareStatement(withdrawSQL);
+            pstmt.setDouble(1, amount);
+            pstmt.setInt(2, fromAcno);
+            pstmt.executeUpdate();
+
+            String depositSQL = "Update PSBank set Amount = Amount + ? where Acno = ?";
+            pstmt = conn.prepareStatement(depositSQL);
+            pstmt.setDouble(1, amount);
+            pstmt.setInt(2, toAcno);
+            pstmt.executeUpdate();
+
+            String sql = "Select Amount From PSBank where Acno = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, fromAcno);
+            ResultSet rs = pstmt.executeQuery();
+            double balanceAmount=0;
+            if( rs.next()){
+                balanceAmount = rs.getDouble("Amount");
+            }
+
+            if( balanceAmount >= 5000){
+                conn.commit();
+                System.out.println("Amount Transferred Successfully!!!");
+            }
+            else{
+                conn.rollback();
+                System.err.println("Insufficient Funds \t\t\t: " + balanceAmount + " Transactions Rollbacked!!!");
+            }
+
+            scanner.close();
+            pstmt.close();
+            conn.close();
+        }
+        catch(Exception ex){
+            System.err.println(ex.getMessage());
+        }
+    }
+
+}
